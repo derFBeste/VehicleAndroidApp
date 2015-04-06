e044976504ff99fa48e66c565101ed7bda98f899
derFBeste <freddybeste@gmail.com>
2015-04-05 21:53:59 -0400

    Intial commit.

---
+++ src/com/example/vehiclefredbesteman/VehicleFredBestemanActivity.java
@@ -0,0 +1,15 @@
+package com.example.vehiclefredbesteman;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class VehicleFredBestemanActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_vehicle_fred_besteman);
+	}
+}
